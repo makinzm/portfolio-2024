0285f09e7dbcd38fbf0e613ca5ead2f9fad906aa
makinzm <yuruseba@yahoo.co.jp>
2023-12-30 23:27:36 +0900

    Updates

--- assets/_id_.Nx4hDDUk.js
+++
@@ -1,1 +0,0 @@
-import{f as s,h as t,o as a,c as n,a as l,t as r,i as c}from"./entry.Y3y6l5LW.js";const m=s({__name:"[id]",setup(p){const e=t();console.log(e),console.log(e.params.id);const o=e.params.id;return(i,u)=>(a(),n("section",null,[l("p",null,"This page will be displayed at the /posts/"+r(c(o))+" route.",1)]))}});export{m as default};
