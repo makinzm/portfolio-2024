045e0d8cba11c9b33c80581bbd550e8ad49e7770
makinzm <yuruseba@yahoo.co.jp>
2023-12-31 14:47:44 +0900

    change: buildAssetsDir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     baseURL: '/portfolio-2024/',
-    buildAssetsDir: 'assets',
+    buildAssetsDir: 'dist',
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
